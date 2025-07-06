2c897284cbec2f895b0f46eab232ef1544278fa9
Jeongmo Seo <wjdah1402@gmail.com>
2025-07-06 15:42:48 +0900

    :sparkles: feat: 인증 방식을 쿠키로 변경

---
+++ src/main/java/org/withtime/be/withtimebe/global/security/constants/AuthenticationConstants.java
@@ -0,0 +1,11 @@
+package org.withtime.be.withtimebe.global.security.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AuthenticationConstants {
+
+    public static final String ACCESS_TOKEN_NAME = "accessToken";
+    public static final String REFRESH_TOKEN_NAME = "refreshToken";
+}

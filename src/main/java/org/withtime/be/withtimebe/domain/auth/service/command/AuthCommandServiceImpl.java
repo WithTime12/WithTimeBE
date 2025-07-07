54910b49a4f119a409488a739c113048612ddc62
Jeongmo Seo <wjdah1402@gmail.com>
2025-07-07 15:11:35 +0900

    :bug: fix: 회원가입 불가 시 에러 처리 추가

--- src/main/java/org/withtime/be/withtimebe/domain/auth/service/command/AuthCommandServiceImpl.java
+++ src/main/java/org/withtime/be/withtimebe/domain/auth/service/command/AuthCommandServiceImpl.java
@@ -7,6 +7,8 @@ import org.withtime.be.withtimebe.domain.auth.converter.AuthConverter;
 import org.withtime.be.withtimebe.domain.auth.dto.request.AuthRequestDTO;
 import org.withtime.be.withtimebe.domain.member.entity.Member;
 import org.withtime.be.withtimebe.domain.member.repository.MemberRepository;
+import org.withtime.be.withtimebe.global.error.code.AuthErrorCode;
+import org.withtime.be.withtimebe.global.error.exception.AuthException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +19,15 @@ public class AuthCommandServiceImpl implements AuthCommandService {
 
     @Override
     public void signUp(AuthRequestDTO.SignUp request) {
-        if (availableSignUp(request)) {
-            Member member = AuthConverter.toLocalMember(request.email(), request.username(), passwordEncoder.encode(request.password()), request.phoneNumber(), request.gender(), request.birth());
-            memberRepository.save(member);
-        }
+        validateSignUp(request);
+
+        Member member = AuthConverter.toLocalMember(request.email(), request.username(), passwordEncoder.encode(request.password()), request.phoneNumber(), request.gender(), request.birth());
+        memberRepository.save(member);
     }
 
-    private boolean availableSignUp(AuthRequestDTO.SignUp request) {
-        return !memberRepository.existsByEmail(request.email());
+    private void validateSignUp(AuthRequestDTO.SignUp request) throws AuthException {
+        if (memberRepository.existsByEmail(request.email())) {
+            throw new AuthException(AuthErrorCode.ALREADY_EXIST_EMAIL);
+        }
     }
 }

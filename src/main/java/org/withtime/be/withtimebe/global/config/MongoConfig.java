3d547a50498418686e0aa691bec5133986c222fc
pywoo <kfdsy0103@naver.com>
2025-07-17 20:42:43 +0900

    ✨  feat: 시간대 설정 및 컨버터 MongoConfig에 등록

---
+++ src/main/java/org/withtime/be/withtimebe/global/config/MongoConfig.java
@@ -0,0 +1,47 @@
+package org.withtime.be.withtimebe.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.withtime.be.withtimebe.global.converter.MongoConverters;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+public class MongoConfig {
+
+	// KST <-> UTC 커스텀 컨버터 빈 등록
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return new MongoCustomConversions(List.of(
+			new MongoConverters.LocalDateToDateKstConverter(),
+			new MongoConverters.LocalTimeToDateKstConverter(),
+			new MongoConverters.DateToLocalDateKstConverter(),
+			new MongoConverters.DateToLocalTimeKstConverter()
+		));
+	}
+
+	// 커스텀 컨버터 등록 및 _class 필드 제거
+	@Bean
+	public MappingMongoConverter mappingMongoConverter(
+		MongoDatabaseFactory mongoDatabaseFactory,
+		MongoMappingContext mongoMappingContext,
+		MongoCustomConversions conversions
+	) {
+		DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+		MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+		converter.setCustomConversions(conversions);
+		return converter;
+	}
+}

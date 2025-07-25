4b7382ddfa1fc3df798d9574a8141d8173a4e6ff
Jeongmo Seo <wjdah1402@gmail.com>
2025-07-25 16:34:24 +0900

    :sparkles: feat: 비밀번호 찾기 API 구현

--- src/main/java/org/withtime/be/withtimebe/domain/auth/service/command/AuthCommandService.java
+++ src/main/java/org/withtime/be/withtimebe/domain/auth/service/command/AuthCommandService.java
@@ -2,10 +2,12 @@ package org.withtime.be.withtimebe.domain.auth.service.command;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.withtime.be.withtimebe.domain.auth.dto.request.AuthRequestDTO;
 
 public interface AuthCommandService {
     void signUp(AuthRequestDTO.SignUp request);
     void reissueToken(HttpServletRequest request, HttpServletResponse response);
     void logout(HttpServletRequest request, HttpServletResponse response);
+    void findPassword(@RequestBody AuthRequestDTO.FindPassword request);
 }

bbac0773cc7b813315744957a65b7737382a708f
pywoo <kfdsy0103@naver.com>
2025-07-27 22:38:34 +0900

    ✨  feat: 시큐리티 인증된 URL 추가

--- src/main/java/org/withtime/be/withtimebe/global/security/SecurityConfig.java
+++ src/main/java/org/withtime/be/withtimebe/global/security/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
             API_PREFIX + "/notices/**",
             API_PREFIX + "/oauth2/**",
             API_PREFIX + "/faqs/**",
+            API_PREFIX + "/logs/keyword/**",
             "/oauth2/authorization/**",
             "/swagger-ui/**",
             "/swagger-resources/**",

8a2c873886f8c0fee0681861f3fa61cdf88212c4
pywoo <kfdsy0103@naver.com>
2025-07-27 22:36:33 +0900

    ✨  feat: KST UTC 변환 컨버터 등록 및 _class 필드 제거

--- src/main/java/org/withtime/be/withtimebe/global/config/MongoConfig.java
+++ src/main/java/org/withtime/be/withtimebe/global/config/MongoConfig.java
@@ -1,8 +1,19 @@
 package org.withtime.be.withtimebe.global.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.withtime.be.withtimebe.global.converter.MongoConverters;
 
 import lombok.AllArgsConstructor;
 
@@ -11,4 +22,31 @@ import lombok.AllArgsConstructor;
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "org.withtime.be.withtimebe.domain.log")
 public class MongoConfig {
+
+	// KST <-> UTC 커스텀 컨버터 빈 등록
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return new MongoCustomConversions(List.of(
+			new MongoConverters.LocalDateToDateKstConverter(),
+			new MongoConverters.LocalTimeToDateKstConverter(),
+			new MongoConverters.LocalDateTimeToDateKstConverter(),
+			new MongoConverters.DateToLocalDateKstConverter(),
+			new MongoConverters.DateToLocalTimeKstConverter(),
+			new MongoConverters.DateToLocalDateTimeKstConverter()
+		));
+	}
+
+	// 커스텀 컨버터 등록 및 _class 필드 제거
+	@Bean
+	public MappingMongoConverter mappingMongoConverter(
+		MongoDatabaseFactory mongoDatabaseFactory,
+		MongoMappingContext mongoMappingContext,
+		MongoCustomConversions conversions
+	) {
+		DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+		MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+		converter.setCustomConversions(conversions);
+		return converter;
+	}
 }

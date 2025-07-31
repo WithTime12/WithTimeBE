41355e8a8b7ddf2f8137ff6036ae8f372759c59d
pywoo <kfdsy0103@naver.com>
2025-08-01 01:15:53 +0900

    🐛 fix: 코드 수정

--- src/main/java/org/withtime/be/withtimebe/domain/log/placecategorylog/service/query/PlaceCategoryLogQueryServiceImpl.java
+++ src/main/java/org/withtime/be/withtimebe/domain/log/placecategorylog/service/query/PlaceCategoryLogQueryServiceImpl.java
@@ -2,7 +2,6 @@ package org.withtime.be.withtimebe.domain.log.placecategorylog.service.query;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -24,8 +23,8 @@ public class PlaceCategoryLogQueryServiceImpl implements PlaceCategoryLogQuerySe
 
 		LocalDate now = LocalDate.now();
 
-		LocalDate startOfWeek = now.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
-		LocalDate endOfWeek = now.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+		LocalDate startOfWeek = now.with(DayOfWeek.MONDAY);
+		LocalDate endOfWeek = now.with(DayOfWeek.SUNDAY);
 
 		return placeCategoryLogRepository.findByDateBetween(startOfWeek, endOfWeek);
 	}

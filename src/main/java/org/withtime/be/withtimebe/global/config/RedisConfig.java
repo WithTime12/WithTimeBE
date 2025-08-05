0476ef9aaf6f39afa26daacdd552e7a0e90328d0
pywoo <kfdsy0103@naver.com>
2025-08-05 16:20:21 +0900

    ✨  feat: Redis 캐시 매니저 설정

--- src/main/java/org/withtime/be/withtimebe/global/config/RedisConfig.java
+++ src/main/java/org/withtime/be/withtimebe/global/config/RedisConfig.java
@@ -1,19 +1,35 @@
 package org.withtime.be.withtimebe.global.config;
 
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.*;
+
+import java.time.Duration;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.withtime.be.withtimebe.global.data.RedisConfigData;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
 
     private final RedisConfigData redisConfigData;
+    private final ObjectMapper objectMapper;
 
     @Bean
     RedisConnectionFactory redisConnectionFactory() {
@@ -27,5 +43,16 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setValueSerializer(RedisSerializer.java());
         return  redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
+            .entryTtl(Duration.ofDays(1L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory())
+            .cacheDefaults(redisCacheConfiguration).build();
     }
 }

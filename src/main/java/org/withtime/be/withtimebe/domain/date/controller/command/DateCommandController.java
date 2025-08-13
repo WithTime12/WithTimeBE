c882a64dbbfcbbdb45a1a3e4413500a318a23cc8
yoojinche <chesunny917@gmail.com>
2025-08-13 12:57:26 +0900

    :sparkles:feat: 데이트 코스 생성 기능 추가

--- src/main/java/org/withtime/be/withtimebe/domain/date/controller/command/DateCommandController.java
+++ src/main/java/org/withtime/be/withtimebe/domain/date/controller/command/DateCommandController.java
@@ -7,11 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.namul.api.payload.response.DefaultResponse;
 import org.springframework.web.bind.annotation.*;
 import org.withtime.be.withtimebe.domain.date.converter.DateConverter;
+import org.withtime.be.withtimebe.domain.date.dto.request.DateRequestDTO;
 import org.withtime.be.withtimebe.domain.date.dto.response.DateResponseDTO;
 import org.withtime.be.withtimebe.domain.date.entity.DateCourseBookmark;
+import org.withtime.be.withtimebe.domain.date.entity.DatePlace;
 import org.withtime.be.withtimebe.domain.date.service.command.DateCommandService;
 import org.withtime.be.withtimebe.domain.member.entity.Member;
 import org.withtime.be.withtimebe.global.security.annotation.AuthenticatedMember;
+
+import java.util.List;
 
 
 @RestController
@@ -21,7 +25,22 @@ public class DateCommandController {
 
     private final DateCommandService dateCommandService;
 
-    @Operation(summary = "데이트 코스 북마크 생성 API by 제인", description = "데이트 코스 북마크 API입니다")
+    @Operation(summary = "사용자 맞춤형 데이트 코스 생성 API by 제인", description = "데이트 코스 생성 API입니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공입니다")
+    })
+    @PostMapping("/")
+    public DefaultResponse<DateResponseDTO.DateCourse> createDateCourse(
+            @RequestBody DateRequestDTO.CreateDateCourse request
+//            @AuthenticatedMember Member member
+    ){
+        List<DatePlace> datePlaces = dateCommandService.createDateCourse(request);
+        DateResponseDTO.DateCourse dateCourse = DateConverter.createDateCourseInfo(datePlaces);
+        return DefaultResponse.created(dateCourse);
+    }
+
+    @Operation(summary = "데이트 코스 북마크 생성 API by 제인",
+            description = "직접 데이트 코스를 찾을 때 사용하는 데이트 코스 북마크 API입니다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공입니다"),
             @ApiResponse(responseCode = "DATE_COURSE404_1",
@@ -37,7 +56,8 @@ public class DateCommandController {
         return DefaultResponse.created(responseDTO);
     }
 
-    @Operation(summary = "데이트 코스 북마크 삭제 API by 제인", description = "데이트 코스 북마크 API입니다")
+    @Operation(summary = "데이트 코스 북마크 삭제 API by 제인",
+            description = "데이트 코스 북마크 API입니다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공입니다"),
             @ApiResponse(responseCode = "DATE_COURSE404_1",
@@ -51,4 +71,22 @@ public class DateCommandController {
         dateCommandService.deleteDateCourseBookmark(dateCourseId, member);
         return DefaultResponse.noContent();
     }
+
+    @Operation(summary = "데이트 코스 북마크 생성 API by 제인",
+            description = "데이트 코스를 생성할 때 사용하는 데이트 코스 북마크 API입니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공입니다"),
+            @ApiResponse(responseCode = "DATE_COURSE404_1",
+                    description = "해당 코스를 찾을 수 없습니다")
+    })
+    @PostMapping("/bookmark")
+    public DefaultResponse<DateResponseDTO.DateCourseBookmark> createDateCourseBookmarkWithGeneratedCourse(
+            @RequestBody DateRequestDTO.SaveDateCourse request,
+            @AuthenticatedMember Member member
+    ){
+        DateCourseBookmark dateCourseBookmarkWithGeneratedCourse = dateCommandService.createDateCourseBookmarkWithGeneratedCourse(request, member);
+        DateResponseDTO.DateCourseBookmark dateCourseBookmarkResponseDTO = DateConverter.createDateCourseBookmarkResponseDTO(dateCourseBookmarkWithGeneratedCourse);
+        return DefaultResponse.created(dateCourseBookmarkResponseDTO);
+    }
+
 }

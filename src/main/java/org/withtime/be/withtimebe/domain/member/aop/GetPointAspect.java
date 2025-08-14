e6a43bbe916fa420b6740d1ebb4eac0d4a915a7e
pywoo <kfdsy0103@naver.com>
2025-08-14 19:53:09 +0900

    🐛 fix: 포인트 변경 감지를 위해 적립 AOP의 순서 조절

--- src/main/java/org/withtime/be/withtimebe/domain/member/aop/GetPointAspect.java
+++ src/main/java/org/withtime/be/withtimebe/domain/member/aop/GetPointAspect.java
@@ -3,6 +3,8 @@ package org.withtime.be.withtimebe.domain.member.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,10 +15,13 @@ import org.withtime.be.withtimebe.domain.member.service.command.MemberCommandSer
 import org.withtime.be.withtimebe.global.security.domain.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
+@Order(Ordered.LOWEST_PRECEDENCE - 1) // 읽기 전용 트랜잭션보다 먼저 실행되도록 하여 포인트 변경 감지되도록
 public class GetPointAspect {
 
 	private final MemberCommandService memberCommandService;
@@ -31,6 +36,7 @@ public class GetPointAspect {
 		// 인증 객체 추출
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if(authentication == null || !authentication.isAuthenticated()) {
+			log.info("[GetPointAspect] 유저의 인증 정보가 존재하지 않아 포인트가 적립되지 않았습니다.");
 			return;
 		}
 		
@@ -39,6 +45,7 @@ public class GetPointAspect {
 		if(userDetails instanceof CustomUserDetails customUserDetails) {
 			Long memberId = customUserDetails.getMember().getId();
 			memberCommandService.addPoint(memberId, point);
+			log.info("[GetPointAspect] {} 님의 포인트 적립에 성공하였습니다.", customUserDetails.getMember().getUsername());
 		}
 	}
 }

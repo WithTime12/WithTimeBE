490016a968b525e39876096c53f15d9307a658b9
pywoo <kfdsy0103@naver.com>
2025-08-14 18:37:01 +0900

    ✨  feat: 인기 키워드 집계 어노테이션 적용 및 확인

--- src/main/java/org/withtime/be/withtimebe/domain/date/service/query/DateQueryServiceImpl.java
+++ src/main/java/org/withtime/be/withtimebe/domain/date/service/query/DateQueryServiceImpl.java
@@ -11,6 +11,7 @@ import org.withtime.be.withtimebe.domain.date.entity.DateCourse;
 import org.withtime.be.withtimebe.domain.date.entity.DateCourseBookmark;
 import org.withtime.be.withtimebe.domain.date.repository.DateCourseBookmarkRepository;
 import org.withtime.be.withtimebe.domain.date.repository.DateCourseRepository;
+import org.withtime.be.withtimebe.domain.log.placecategorylog.annotation.LogPlaceCategory;
 import org.withtime.be.withtimebe.domain.member.entity.Member;
 
 import java.util.List;
@@ -22,10 +23,12 @@ public class DateQueryServiceImpl implements DateQueryService {
 
     private final DateCourseRepository dateCourseRepository;
 
+    @LogPlaceCategory
     public Page<DateCourse> findDateCourses(DateRequestDTO.DateCourseSearchCond dateCourseSearchCond, Pageable pageable){
         return dateCourseRepository.searchDateCourseByApplyPage(dateCourseSearchCond, pageable);
     }
 
+    @LogPlaceCategory
     public Page<DateCourse> findDateCourseBookmarks(DateRequestDTO.DateCourseSearchCond dateCourseSearchCond, Pageable pageable, Member member){
         return dateCourseRepository.searchDateCourseBookmarkByMemberAndApplyPage(dateCourseSearchCond, member, pageable);
     }

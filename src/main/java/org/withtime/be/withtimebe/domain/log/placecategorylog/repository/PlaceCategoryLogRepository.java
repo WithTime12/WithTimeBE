c655584da90e01852d5689da08950d8626e78f88
pywoo <kfdsy0103@naver.com>
2025-08-18 17:36:40 +0900

    🐛 fix: 양 끝 날짜가 포함되어 조회되도록 수정

--- src/main/java/org/withtime/be/withtimebe/domain/log/placecategorylog/repository/PlaceCategoryLogRepository.java
+++ src/main/java/org/withtime/be/withtimebe/domain/log/placecategorylog/repository/PlaceCategoryLogRepository.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.withtime.be.withtimebe.domain.log.placecategorylog.model.PlaceCategoryLog;
 
 public interface PlaceCategoryLogRepository extends MongoRepository<PlaceCategoryLog, String> {
+	@Query("{ 'date': { $gte: ?0, $lte: ?1 } }")
 	List<PlaceCategoryLog> findByDateBetween(LocalDate startDate, LocalDate endDate);
 	List<PlaceCategoryLog> findByDateAndPlaceCategoryLabelIn(LocalDate date, List<String> placeCategoryLabel);
 }
